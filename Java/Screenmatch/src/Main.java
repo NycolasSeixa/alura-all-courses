35693858f5eb0567a6fde8eb3787ad9db7fbfce7
nyccolas <pradoseixa@gmail.com>
2026-05-13 22:17:08 -0300

    movie class

---
+++ Java/Screenmatch/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        Movie SpiderMan = new Movie();
+        SpiderMan.name = "SpiderMan";
+        SpiderMan.realeaseYear = 2002;
+        SpiderMan.totalMinutes = 121;
+
+        System.out.println(SpiderMan.name);
+        System.out.println(SpiderMan.realeaseYear);
+    }
+}   
---
+++ Java/Screenmatch/src/Movie.java
@@ -0,0 +1,8 @@
+public class Movie {
+    String name;
+    int realeaseYear;
+    boolean includedInPlan;
+    double rating;
+    int totalRating;
+    int totalMinutes;
+}
